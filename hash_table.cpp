733bc3ebb5a03fe757e86b520bd0f894177353c0
Henry <henryhjia@gmail.com>
2017-09-26 17:00:07 -0500

    Update hash_table.cpp

--- hash_table.cpp
+++ hash_table.cpp
@@ -1,77 +1,81 @@
 #include <iostream>
-
+#include <cstdio>
 using namespace std;
 
 struct ListNode {
-	  int value;
-	  ListNode *next;
+	int value;
+	ListNode *next;
 };
 
-ListNode* create_node(int val) {
-	  ListNode *new_node = new ListNode;
-	  new_node->next = nullptr;
-	  new_node->value = val;
+ListNode* createNode(int val) {
+	ListNode *new_node = new ListNode;
+	new_node->next = nullptr;
+	new_node->value = val;
     cout << "newNode: " << new_node << " value=" << val << endl;
-	  return new_node;
+	return new_node;
 }
 
-int get_location(ListNode* node_ptr) {
-	  int val = node_ptr->value;
-	  return val % 10;
+int getLocation(ListNode* node_ptr) {
+	int val = node_ptr->value;
+	return val % 10;
 }
 
-void insert(ListNode* v[], ListNode *newNode, int index) {
-	  if (v[index] == nullptr) {
-		    v[index] = newNode;
-	  }
-	  else {
-		    ListNode *nodePtr = v[index];
-		    ListNode *prevPtr = nullptr;
-		    while (nodePtr != nullptr && nodePtr->value < newNode->value) {
-			      prevPtr = nodePtr;
-			      nodePtr = nodePtr->next;
-		    }
-		    if (prevPtr == nullptr) {
-			      v[index] = newNode;
-			      newNode->next = nodePtr;
-		    }
-		    else {
-			      prevPtr->next = newNode;
-			      newNode->next = nodePtr;
-		    }
-	  }
+int getHashKey(int num) {   
+    return num % 10;
 }
 
-void print_list(ListNode* v[]) {
-	  ListNode *nodePtr = nullptr;
+void insertNode(ListNode* v[], ListNode *newNode, int index) {
+	if (v[index] == nullptr) {
+		v[index] = newNode;
+	}
+	else {
+		ListNode *nodePtr = v[index];
+		ListNode *prevPtr = nullptr;
+		while (nodePtr != nullptr && nodePtr->value < newNode->value) {
+			prevPtr = nodePtr;
+			nodePtr = nodePtr->next;
+		}
+		if (prevPtr == nullptr) {
+			v[index] = newNode;
+			newNode->next = nodePtr;
+		}
+		else {
+			prevPtr->next = newNode;
+			newNode->next = nodePtr;
+		}
+	}
+}
 
-	  for (int i = 0; i < 10; i++) {
-		    cout << "[" << i << "]: " ;
-		    nodePtr = v[i];
-		    while (nodePtr != nullptr) {
-			      cout  << nodePtr->value << " ";
-			      nodePtr = nodePtr->next;
-		    }
+void printHashTable(ListNode* v[]) {
+	ListNode *nodePtr = nullptr;
+
+	for (int i = 0; i < 10; i++) {
+		cout << "[" << i << "]: " ;
+		nodePtr = v[i];
+		while (nodePtr != nullptr) {
+			cout  << nodePtr->value << " ";
+			nodePtr = nodePtr->next;
+		}
         cout << endl;
-	  }
+	}
 }
 
-void delete_a_node(ListNode* v[], int val) {   
+void deleteNode(ListNode* v[], int val) {   
     ListNode *prevPtr;
     ListNode *nodePtr;
     
-    for (int i = 0; i < 10; i++) {
+	for (int i = 0; i < 10; i++) {
         nodePtr = v[i];   
         
         if(nodePtr == nullptr) continue;
         
         // First node
-	      if (nodePtr->value == val) {
-			      v[i] = nodePtr->next;
+	    if (nodePtr->value == val) {
+			v[i] = nodePtr->next;
             cout << "delete: " << nodePtr;
-			      delete nodePtr;
-			      continue;
-		    }
+			delete nodePtr;
+			continue;
+		}
         
         // Rest of nodes
         while(nodePtr != nullptr && nodePtr->value != val) {
@@ -89,8 +93,7 @@ void delete_a_node(ListNode* v[], int val) {
     }
 }
 
-
-void delete_all_node(ListNode* v[]) {
+void destroyHashTable(ListNode* v[]) {
 	for (int i = 0; i < 10; i++) {
         ListNode *nodePtr = v[i];        
         while(nodePtr != nullptr) {
@@ -104,67 +107,98 @@ void delete_all_node(ListNode* v[]) {
     }
 }
 
-void hash_table() {
-	  cout << "Week 6: Hash Table" << endl;
+bool searchNode(ListNode* v[], int val) {
 
-	  ListNode* arr[10] = {0};
-	  ListNode* node = nullptr;
-	  int index;
+	for (int i = 0; i < 10; i++) {
+        ListNode *nodePtr = v[i];        
+        while(nodePtr != nullptr) {
+            if(nodePtr->value != val) {
+                nodePtr = nodePtr->next;
+            }
+            else {
+                // found the node
+                return true;
+            }
+        }        
+    }    
+    return false;
+}
+
+
+void hash_table() {
+	cout << "Week 6: Hash Table" << endl;
+
+	ListNode* arr[10] = {0};
+	ListNode* node = nullptr;
+	int index;
     int num;
     
-	  char ch = 'q';
-	  do {
-		    cout << "What do you want to do?" << endl;
-		    cout << "a - add" << endl;
-        cout << "b - delete a node" << endl;
-		    cout << "d - delete all" << endl;
-		    cout << "p - print" << endl;
-		    cout << "q - quit" << endl;
+	char ch = 'q';
+	do {
+		cout << "What do you want to do?" << endl;
+		cout << "a - add a node" << endl;
+        cout << "d - delete a node" << endl;
+		cout << "x - destroy hash table" << endl;
+        cout << "s - seach a node" << endl;
+		cout << "p - print hash table" << endl;
+		cout << "q - quit" << endl;
 
-		    cin >> ch;
-		    cout << "ch=" << ch << endl;
+		cin >> ch;
+		cout << "ch=" << ch << endl;
 
-		    switch (ch) {
-		        case 'a': 
-                cout << "add" << endl;
-                cout << "enter a value to insert:" << endl;
-                cin >> num;
-			          node = create_node(num);
-			          index = get_location(node);
-			          insert(arr, node, index);
+		switch (ch) {
+		case 'a': 
+            cout << "add" << endl;
+            cout << "enter a value to insert:" << endl;
+            cin >> num;
+			node = createNode(num);
+			index = getLocation(node);
+			insertNode(arr, node, index);
 
-			      break;
+			break;
                 
-		        case 'b':
-                cout << "enter a value to delete" << endl;
-                cin >> num;
-                delete_a_node(arr, num);
+		case 'd':
+            cout << "enter a value to delete" << endl;
+            cin >> num;
+            deleteNode(arr, num);
             break;
                 
-		        case 'd':
-                cout << "delete all node" << endl;
-                delete_all_node(arr);
+		case 'x':
+            cout << "delete all node" << endl;
+            destroyHashTable(arr);
             break;
 
-		        case 'p': cout << "print" << endl;
-			          print_list(arr);
-			      break;
+        case 's':
+            cout << "search a node:" << endl;
+            cout << "please enter a value:" << endl;
+            cin >> num;
+            if(searchNode(arr, num)) {
+                cout << "node (value=" << num << ") found" << endl;
+            }
+            else cout << "node (value=" << num << ") not found" << endl;
+                
+            break;
+                
+		case 'p': cout << "print" << endl;
+			printHashTable(arr);
+			break;
 
-		        case 'q': cout << "quit" << endl;
-                delete_all_node(arr);
-			      break;
+		case 'q': cout << "quit" << endl;
+            destroyHashTable(arr);
+			break;
 
-		        default:
-			          cout << "Invalid" << endl;
-			      break;
-		    }
+		default:
+			cout << "Invalid" << endl;
+			break;
+		}
 	} while (ch != 'q');
     
 }
 
+
 int main() {
     // Complete the code.
-    hash_table();
-	  return EXIT_SUCCESS;                
+    //hash_table();
+	return EXIT_SUCCESS;                
 
 }
