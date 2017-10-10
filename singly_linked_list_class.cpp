a0b85430eb559f2afa33214246b03c7ffdb90993
Henry <henryhjia@gmail.com>
2017-10-10 10:17:58 -0500

    Update singly_linked_list_class.cpp

--- singly_linked_list_class.cpp
+++ singly_linked_list_class.cpp
@@ -1,7 +1,12 @@
-    #include <iostream>
-    using namespace std;
+#include <iostream>
+using namespace std;
     
-    int main() {
+struct ListNode {
+    int value;
+    ListNode* next;
+};
+
+int main() {
     
     // Linked List
     ListNode *head = nullptr;
