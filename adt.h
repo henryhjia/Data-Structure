10db255fd3030042184db4817af0b792e2d85da1
Henry <henryhjia@gmail.com>
2017-10-17 15:41:28 -0500

    Update adt.h

--- adt.h
+++ adt.h
@@ -1,24 +1,26 @@
+#ifndef __MYLIST_
+#define _MYLIST_
 // adt.h
-
-struct ListNode {
-    int value;
-    ListNode* next;
-};
-
 MyList
 {
-	public:
-		MyList();
-		void insertNode(int val);
-		void deleteNode(int val);
-		void printMyList();
-		bool searchNode(int val);
-		bool isEmpty();
-		void insertHead(int val);
-		void deleteTail();
-		void deleteHead();
-		
-	private:	
-		ListNode* head;
-
+    public:
+	MyList();
+	void insertNode(int val);
+	void deleteNode(int val);
+	void printMyList();
+	bool searchNode(int val);
+	bool isEmpty();
+	void insertHead(int val);
+	void deleteTail();
+	void deleteHead();
+	
+    private:
+	struct ListNode {
+	    int value;
+    	    ListNode* next;
+	};
+	
+	ListNode* head;
 };
+
+#endif
