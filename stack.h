3bc7ddf74ae360a260ac22732d7caa7be2592350
Henry <henryhjia@gmail.com>
2017-10-17 09:31:12 -0500

    Create stack.h

---
+++ stack.h
@@ -0,0 +1,18 @@
+#ifndef _MYSTACK_
+#define _MYSTACK_
+#pragma once
+#include "listADT.h"
+
+class MyStack : public MyList
+{
+private:
+
+public:
+	MyStack();
+
+	void push(int num);
+	void pop();
+
+};
+
+#endif
