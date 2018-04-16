fb3e7dba466b0d0f218317c8cc234af2f2103e3b
Henry <henryhjia@gmail.com>
2018-04-15 21:26:25 -0500

    Create list_class.h

---
+++ list_class.h
@@ -0,0 +1,35 @@
+// list_class.h
+#ifndef _LISTADT_H_
+#define _LISTADT_H_
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+class MyList
+{
+    public:
+        MyList();
+        ~MyList();
+
+        void insertNode(int val);
+        void deleteNode(int val);
+        void printMyList();
+        bool searchNode(int val);
+        bool isEmpty();
+
+        void insertHead(int val);
+        void deleteTail();
+        void deleteHead();
+
+    private:
+
+        struct ListNode {
+            int value;
+            ListNode* next;
+        };
+
+        ListNode* head;
+};
+
+
+#endif
