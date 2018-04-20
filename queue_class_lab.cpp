7c1df2b6d3c8d6ff0f688f5a838b3ae7529bcddb
Henry <henryhjia@gmail.com>
2018-04-20 11:24:42 -0500

    Update queue_class_lab.cpp

--- queue_class_lab.cpp
+++ queue_class_lab.cpp
@@ -5,24 +5,52 @@ using namespace std;
 
 int main() {
 
-        MyQueue q;
-        q.push(10);
-        q.push(20);
-        q.push(30);
-        q.printMyQueue();
+    MyQueue q;
 
-        MyQueue::ListNode* nodePtr;
-        nodePtr = q.pop();
-        cout << "poped node= " << nodePtr->value << endl;
-        q.printMyQueue();
+    int value = 0;
+    char ch = 'q';
+    MyQueue::ListNode* nodePtr;
 
-        nodePtr = q.pop();
-        cout << "poped node= " << nodePtr->value << endl;
-        q.printMyQueue();
+    do {
 
-        nodePtr = q.pop();
-        cout << "poped node= " << nodePtr->value << endl;
-        q.printMyQueue();
+        cout << "a - push" << endl;
+        cout << "d - pop" << endl;
+        cout << "p - print" << endl;
+        cout << "q - quit" << endl;
+
+        cin >> ch;
+        cout << "ch=" << ch << endl;
+
+        switch (ch) {
+
+            case 'a':
+                cout << "Enter an integer to push to queue:" << endl;
+                cin >> value;
+                q.push(value);
+                break;
+
+            case 'd':
+                cout << "pop a queue" << endl;
+                nodePtr = q.pop();
+                cout << "poped node= " << nodePtr->value << endl;
+                cout << "delte the poped node=" << nodePtr << endl;
+                delete nodePtr;
+                break;
+
+            case 'p':
+                cout << "print queue" << endl;
+                q.print();
+                break;
+
+            case 'q':
+                cout << "quit" << endl;
+                break;
+
+            default:
+                cout << "invalid entry" << endl;
+            }
+
+        } while (ch != 'q');                        
 
         return 0;
 }
